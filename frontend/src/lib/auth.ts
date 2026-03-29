719cf248ca9e9d9655b37c3ac4c776bb1ed6fdb6
Thành Nguyễn <nguyennhuthanh0104@gmail.com>
2026-03-29 17:35:45 +0700

    feat: add authentication method

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -19,7 +19,20 @@ export const auth = betterAuth({
         provider: "postgresql",
     }),
     emailAndPassword: {
-        enabled: true,
+        enabled: false,
+        requireEmailVerification: true,
+    },
+    socialProviders: {
+      github: {
+        clientId: env.GITHUB_CLIENT_ID,
+        clientSecret: env.GITHUB_CLIENT_SECRET,
+      },
+      google: {
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+        prompt: "select_account consent",
+        accessType: "offline",
+      },
     },
     plugins: [
       polar({
